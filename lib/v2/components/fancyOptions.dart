71a36bcab3a27151fd597f772a919b28698db906
microsomes <tayyabdev@outlook.com>
2020-07-19 01:45:56 +0100

    saving default page works

--- lib/v2/components/fancyOptions.dart
+++ lib/v2/components/fancyOptions.dart
@@ -4,10 +4,14 @@ class FancyOptions extends StatefulWidget{
 
   String title;
   bool isSelected;
+  String assetRoute;
+  Function clicked;
 
     FancyOptions({
       @required this.title,
-      @required this.isSelected
+      @required this.isSelected,
+      @required this.assetRoute,
+      @required this.clicked
     });
 
   @override
@@ -21,33 +25,55 @@ class FancyOptionsState extends State<FancyOptions>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      height: 150,
-      width: MediaQuery.of(context).size.width/1.2,
-      decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(20)
-      ),
-        child:Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-               Padding(
-                 padding: const EdgeInsets.all(20),
-                 child: Container(
-                   height: 10,
-                   width: 10,
-                  decoration: new BoxDecoration(
-                  color: widget.isSelected== true ? Colors.white : Colors.black,
-                  shape: BoxShape.circle,
-              ),
-                 ),
-               ),
-               Text(widget.title,style: TextStyle(color: Colors.white),)
-              ],
-            )
-          ],
+    return InkWell(
+      onTap: (){
+        widget.clicked();
+      },
+          child: Container(
+        height: 150,
+        width: MediaQuery.of(context).size.width/1.2,
+        decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: AssetImage(widget.assetRoute),
+              fit:BoxFit.cover
+            ),
+            boxShadow: [
+             widget.isSelected== true ? BoxShadow(
+                color: Colors.black,
+                offset: new Offset(3.0, 3.0),
+            ): BoxShadow(),
+            ]
         ),
+          child:Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                 Padding(
+                   padding: const EdgeInsets.all(20),
+                   child: Container(
+                     height: 10,
+                     width: 10,
+                    decoration: new BoxDecoration(
+                    color: widget.isSelected== true ? Colors.white : Colors.transparent,
+                    shape: BoxShape.circle,
+                ),
+                   ),
+                 ),
+                 Container(
+                   decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)
+                   ),
+                   padding: EdgeInsets.all(4),
+                   child: Text(widget.title,textAlign: TextAlign.center,style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20),),
+                 )
+                ],
+              )
+            ],
+          ),
+      ),
     );
   }
 }
--- lib/v2/pages/landingPage.dart
+++ lib/v2/pages/landingPage.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:nxclone/v2/components/fancyOptions.dart';
 import 'package:nxclone/v2/main/bar.dart';
+import 'package:sqflite/sqflite.dart';
 
 
 class LandingPage extends StatefulWidget{
@@ -13,6 +16,90 @@ class LandingPage extends StatefulWidget{
 }
 
 class LandingPageState extends State<LandingPage>{
+
+    FancyOptions option1;
+    FancyOptions option2;
+
+    bool isShowing=true;
+
+
+     saveOption(String key,String val) async {
+      var db= await openDatabase("main.db");
+      db.execute("CREATE TABLE IF NOT EXISTS config ( id integer  PRIMARY KEY AUTOINCREMENT, key text, val text)");
+     var iid= await db.rawInsert("INSERT INTO config(key, val) VALUES(?, ?)",[key,val]);
+     print(iid);
+     return iid;
+    }
+
+   Future restoreOption(String key) async {
+        var db= await openDatabase("main.db");
+        List<Map> list = await db.rawQuery('SELECT * FROM config WHERE key=? ORDER BY id DESC limit 1',[key]);
+        return list;
+    }
+
+
+    resetState() async {
+      setState(() {
+              isShowing=false;
+            });
+      await new Future.delayed(const Duration(milliseconds : 100));
+            setState(() {
+              isShowing=true;
+            });
+    }
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    option1= FancyOptions(title: "Home Page",isSelected: true,assetRoute:"images/home1.png" ,clicked: () async {
+                this.saveOption("defaulthomepage", "home");
+            setState(() {
+              option2.isSelected=false;
+              option1.isSelected=true;   
+              isShowing=false;               
+            });
+             resetState();
+          },);
+
+        option2=  FancyOptions(title: "Ticket only",isSelected: false,assetRoute:"images/home2.png",clicked: ()async {
+
+             this.saveOption("defaulthomepage", "ticket");
+            
+            setState(() {
+              option1.isSelected=false;
+            option2.isSelected=true;  
+            isShowing=false;
+             });
+              resetState();
+          },);
+
+        restoreOption("defaulthomepage").then((value) {
+
+            if(value.length==0){
+
+            }else{
+
+            if(value[0]['val']=="home"){
+              print("home page");
+              option2.isSelected=false;
+              option1.isSelected=true;  
+            }else{
+              option1.isSelected=false;
+              option2.isSelected=true;  
+            }
+
+         resetState();
+            }
+
+
+        });
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -34,12 +121,12 @@ class LandingPageState extends State<LandingPage>{
           ) ,
           ),
           ),
+          SizedBox(height: 10,),
+          Text("---"),
           SizedBox(height: 20,),
-          FancyOptions(title: "Home Page",isSelected: true,),
-           SizedBox(height: 10,),
-          FancyOptions(title: "Ticket only",isSelected: false,)
-
-
+        isShowing==true?  option1.createElement().widget:Container(),
+          SizedBox(height: 20,),
+         isShowing==true ? option2.createElement().widget: Container()
         ],
       )
     );
