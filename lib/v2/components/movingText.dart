af244d2606e9f65e83ee417ebacb60742e048516
microsomes <tayyabdev@outlook.com>
2020-07-20 04:26:34 +0100

    finish

---
+++ lib/v2/components/movingText.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MovingText extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
+
+class MovingTextState extends State<MovingText>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
