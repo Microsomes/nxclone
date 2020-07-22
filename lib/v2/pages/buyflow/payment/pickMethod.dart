fc4e14974cb7e47238c7aa9abedc5db764ed66ae
microsomes <tayyabdev@outlook.com>
2020-07-22 12:00:54 +0100

    ticket selection

--- lib/v2/pages/buyflow/payment/pickMethod.dart
+++ lib/v2/pages/buyflow/payment/pickMethod.dart
@@ -40,7 +40,60 @@ class PickPaymentMethodAndConfirmItem extends StatelessWidget{
                             TextStyle(color: Color.fromRGBO(189, 156, 106, 1)),
                       ),
                     ),
-                  )
+
+                    
+
+                  ),
+                  body: Column(
+                    children: <Widget>[
+                        Container(
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                height: 9,
+                                width: MediaQuery.of(context).size.width/2,
+                                color: Color.fromRGBO(187, 157, 107, 1),
+                              ),
+                              Container(
+                                height: 9,
+                                width: MediaQuery.of(context).size.width/2,
+                                color: Color.fromRGBO(169, 27, 26, 1),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 40,
+                          width: MediaQuery.of(context).size.width,
+                          color: Color.fromRGBO(169, 27, 26, 1),
+                          child: Row(
+                            children: <Widget>[
+                              SizedBox(width: 15,),
+                              Text("West Midlands Single Journey",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w300
+                              ),
+                              ),
+                              Expanded(child: Text(""),),
+                              Text("1x £2.50",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700
+                              ),
+                              ),
+                              SizedBox(width: 15,)
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 36,
+                          width: MediaQuery.of(context).size.width,
+                          color: Color.fromRGBO(123, 26, 17, 1),
+                        )
+
+                    ],
+                  ),
     );
   }
 
