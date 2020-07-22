cb474242703eb9d522a6746dfdacbd592f2c0593
microsomes <tayyabdev@outlook.com>
2020-07-22 12:07:23 +0100

    payment method

--- lib/v2/pages/buyflow/payment/pickMethod.dart
+++ lib/v2/pages/buyflow/payment/pickMethod.dart
@@ -79,7 +79,7 @@ class PickPaymentMethodAndConfirmItem extends StatelessWidget{
                               Text("1x £2.50",
                               style: TextStyle(
                                 color: Colors.white,
-                                fontWeight: FontWeight.w700
+                                fontWeight: FontWeight.w600
                               ),
                               ),
                               SizedBox(width: 15,)
@@ -90,6 +90,40 @@ class PickPaymentMethodAndConfirmItem extends StatelessWidget{
                           height: 36,
                           width: MediaQuery.of(context).size.width,
                           color: Color.fromRGBO(123, 26, 17, 1),
+                          child: Row(
+                            children: <Widget>[
+                              SizedBox(width: 15,),
+                              Text("Total",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold
+                              ),),
+                              Expanded(child: Text(""),),
+                              Text("£2.50",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 17
+                              ),
+                              ),
+                              SizedBox(width: 14,)
+
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 24,
+                          color: Color.fromRGBO(253, 237, 237, 1),
+                          child: Row(
+                            children: <Widget>[
+                              SizedBox(width: 15,),
+                              Text("Payment methods",
+                              style: TextStyle(
+                                color: Color.fromRGBO(75, 8, 8, 1),
+                                fontWeight: FontWeight.bold
+                              ),)
+                            ],
+                          ),
                         )
 
                     ],
