bd0b0b7dbd92dbf64ad9592110c915bd94670893
microsomes <tayyabdev@outlook.com>
2020-07-22 02:33:45 +0100

    added all ticket types for west Midlands

--- lib/v2/pages/buyflow/tickettypes.dart
+++ lib/v2/pages/buyflow/tickettypes.dart
@@ -21,6 +21,73 @@ class BuyTicketTypesState extends State<BuyTicketTypes>{
     // TODO: implement initState
     super.initState();
 
+    ticketTypes=List();
+
+
+      ticketTypes.add({
+        "title":"Single Journey",
+        "subtitle":"Any bus acorss our network",
+        "price":"2.40",
+        "state":"West Midlands",
+        "info":[
+          "Includes travel on all our busss acorss the West Midlands, Black Country & Coventry."
+        ]
+      });
+      ticketTypes.add({
+        "title":"DaySaver",
+        "subtitle":"Any busses across our network",
+        "price":"4.60",
+        "state":"West Midlands",
+        "info":[
+            "Includes travel on all our busss acorss the West Midlands, Black Country & Coventry."
+        ]
+      });
+      ticketTypes.add({
+        "title":"DaySaver after 9.30am (Mon-Fri)",
+        "subtitle":"Any busses across our network",
+        "price":"4.00",
+        "state":"West Midlands",
+        "info":[
+            "Includes travel on all our busss acorss the West Midlands, Black Country & Coventry."
+        ]
+      });
+      ticketTypes.add({
+        "title":"Daysaver (Sat-Sun)",
+        "subtitle":"Any busses across our network",
+        "price":"4.00",
+        "state":"West Midlands",
+        "info":[
+            "Includes travel on all our busss acorss the West Midlands, Black Country & Coventry."
+        ]
+      });
+      ticketTypes.add({
+        "title":"Evening Saver after 6pm",
+        "subtitle":"Any busses across our network",
+        "price":"3.00",
+        "state":"West Midlands",
+        "info":[
+            "Includes travel on all our busss acorss the West Midlands, Black Country & Coventry."
+        ]
+      });
+      ticketTypes.add({
+        "title":"Group Daysaver",
+        "subtitle":"Any busses across our network",
+        "price":"7.00",
+        "state":"West Midlands",
+        "info":[
+            "Includes travel on all our busss acorss the West Midlands, Black Country & Coventry."
+        ]
+      });
+      ticketTypes.add({
+        "title":"Group Daysaver after 6pm",
+        "subtitle":"Any busses across our network",
+        "price":"7.00",
+        "state":"West Midlands",
+        "info":[
+             "Includes travel on all our busss acorss the West Midlands, Black Country & Coventry."
+        ]
+      });
+
 
 
   }
