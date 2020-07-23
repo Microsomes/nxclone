a1ab690ddceb5701764ec8e652432cd4e7592d6f
microsomes <tayyabdev@outlook.com>
2020-07-23 16:12:28 +0200

    optimised for other states

--- lib/v2/components/nxsig.dart
+++ lib/v2/components/nxsig.dart
@@ -1,36 +1,29 @@
 import 'package:flutter/material.dart';
 
-class Nxsig extends StatefulWidget{
-
+class Nxsig extends StatefulWidget {
   final bool isRounded;
 
   final String state;
 
-  Nxsig({
-    @required this.isRounded,
-    @required this.state
-  });
-
-    
+  Nxsig({@required this.isRounded, @required this.state});
 
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return NxsigState();
   }
-
 }
 
-class NxsigState extends State<Nxsig>{
+class NxsigState extends State<Nxsig> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    var state= widget.state;
+    var state = widget.state;
 
     return Container(
       height: 140,
-      width: MediaQuery.of(context).size.width*0.785,
+      width: MediaQuery.of(context).size.width * 0.785,
       color: Color.fromRGBO(248, 233, 226, 1),
       child: Column(
         children: <Widget>[
@@ -39,65 +32,76 @@ class NxsigState extends State<Nxsig>{
             decoration: BoxDecoration(
                 color: Color.fromRGBO(140, 36, 40, 1),
                 borderRadius: BorderRadius.only(
-                  topRight: Radius.circular( widget.isRounded==true ? 10 : 0),
-                  topLeft: Radius.circular( widget.isRounded==true ? 10 : 0)
-                )
-            ),
-             child: Row(
+                    topRight:
+                        Radius.circular(widget.isRounded == true ? 10 : 0),
+                    topLeft:
+                        Radius.circular(widget.isRounded == true ? 10 : 0))),
+            child: Row(
               children: <Widget>[
-                SizedBox(width: 15,),
-                Text("$state",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
+                SizedBox(
+                  width: 15,
                 ),
+                Text(
+                  "$state",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
                 ),
-                Expanded(child: Container(),),
-                Text("mTicket",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold
+                Expanded(
+                  child: Container(),
                 ),
+                Text(
+                  "mTicket",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
                 ),
-                SizedBox(width: 10,)
+                SizedBox(
+                  width: 10,
+                )
               ],
             ),
           ),
-          Expanded(child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-            SizedBox(width: 30,),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("national",
-                  style: TextStyle(color: Color.fromRGBO(205, 192, 201, 1),
-                  letterSpacing: 0.1,
-                  fontWeight: FontWeight.w500),),
-                  Text("express",style: TextStyle(color: Color.fromRGBO(205, 192, 201, 1),
-                  height: 0.55,
-                  letterSpacing: 0.1,
-                  fontWeight: FontWeight.w500),),
-                ],
-
-              ),
-              SizedBox(width: 5,),
-              Text("$state",
-              style: TextStyle(
-                color: Color.fromRGBO(239, 193, 193, 1),
-                fontSize: 27,
-                fontWeight: FontWeight.bold
-              ),)
-
-            ],
-            
-
-          ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  width: 30,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "national",
+                      style: TextStyle(
+                          color: Color.fromRGBO(205, 192, 201, 1),
+                          letterSpacing: 0.1,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      "express",
+                      style: TextStyle(
+                          color: Color.fromRGBO(205, 192, 201, 1),
+                          height: 0.55,
+                          letterSpacing: 0.1,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ],
+                ),
+                Expanded(
+                                  child: Text(
+                    "$state",
+                        textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Color.fromRGBO(239, 193, 193, 1),
+                        fontSize: 27,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
           ),
         ],
       ),
     );
-   
   }
-
 }
