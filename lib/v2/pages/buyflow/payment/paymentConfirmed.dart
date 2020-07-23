e99e8bf906daa9212af7eb55f6c7c0b080fff063
microsomes <tayyabdev@outlook.com>
2020-07-23 01:23:18 +0100

    partial payment success page

--- lib/v2/pages/buyflow/payment/paymentConfirmed.dart
+++ lib/v2/pages/buyflow/payment/paymentConfirmed.dart
@@ -21,6 +21,45 @@ class PaymentConfirmed extends StatelessWidget{
           ),
         ),
       ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            height: MediaQuery.of(context).size.height*0.7,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+               children: <Widget>[
+                Text("Transaction",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20
+                ),
+                ),
+                SizedBox(height: 2,),
+                Text("Successful",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20
+                ),
+                ),
+                SizedBox(height: 3,),
+                Icon(Icons.check,size: 100,color: Color.fromRGBO(81, 164, 27, 1),),
+                SizedBox(height: 20,),
+                Text("Please activate your ticket just \nbefore boarding",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15
+                ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 
