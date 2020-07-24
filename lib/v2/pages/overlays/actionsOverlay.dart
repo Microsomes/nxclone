59fc8074e865026a3daa9f6c81db58a7fdeb7d02
microsomes <tayyabdev@outlook.com>
2020-07-24 12:11:18 +0100

    overlay working

--- lib/v2/pages/overlays/actionsOverlay.dart
+++ lib/v2/pages/overlays/actionsOverlay.dart
@@ -1,57 +1,57 @@
-
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-class ActionOverlay{
-
-
-  void display(BuildContext context){
-      showModalBottomSheet(
-      isScrollControlled: true,
-      backgroundColor: Colors.transparent,
+class ActionOverlay {
+  void display(BuildContext context) {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        backgroundColor: Colors.transparent,
         context: context,
         builder: (ctx) {
           return Container(
+            padding: EdgeInsets.all(20),
             decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                topRight: Radius.circular(12)
-              )
-            ),
-            height: MediaQuery.of(context).size.height  * 0.37,
-            
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(12),
+                    topRight: Radius.circular(12))),
+            height: MediaQuery.of(context).size.height * 0.37,
             child: Center(
-              child:Column(
-                children: <Widget>[
-
-                  Expanded(
-                    child: ListView(
-                     children: <Widget>[
-                       ListTile(
-                        leading: Icon(Icons.launch),
-                        title: Text("NX Rewards Cashback"),
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.restore),
-                        title: Text("Purchase Again"),
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.send),
-                        title: Text("Request ticket receipt"),
-                      ),ListTile(
-                        leading: Icon(Icons.close),
-                        title: Text("Close"),
-                      )
-                     ]
+                child: Column(
+              children: <Widget>[
+                Expanded(
+                    child: ListView(children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.launch),
+                    title: Align(
+                        alignment: Alignment(-1.5, 0),
+                        child: Text("NX Rewards Cashback")),
+                  ),
+                  ListTile(
+                    leading: Transform.rotate(
+                        angle: 45 * math.pi / 180, child: Icon(Icons.replay)),
+                    title: Align(
+                        alignment: Alignment(-1.5, 0),
+                        child: Text("   Purchase Again")),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.send),
+                    title: Align(
+                        alignment: Alignment(-1.5, 0),
+                        child: Text(" Request ticket receipt")),
+                  ),
+                  ListTile(
+                    onTap: (){
+                                Navigator.pop(context);
+                    },
+                      leading: Icon(Icons.close),
+                      title: Align(
+                          alignment: Alignment(-1.5, 0), child: Text("       Close")),
                     )
-                    
-                  )
-                 
-                ],
-              )
-            ),
+                ]))
+              ],
+            )),
           );
         });
   }
-
 }
