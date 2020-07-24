0bb311f782d115334733e957a89633748c2c0233
microsomes <tayyabdev@outlook.com>
2020-07-24 09:26:20 +0100

    ticket detail overlay

---
+++ lib/v2/pages/overlays/ticketDetail.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class TicketDetail extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return TicketDetailState();
+  }
+
+}
+
+class TicketDetailState extends State<TicketDetail>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar()
+    );
+  }
+
+}
