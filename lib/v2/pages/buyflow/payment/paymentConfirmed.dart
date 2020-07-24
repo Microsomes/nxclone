df7a63c537ae53cd5cf494f69346edcadb08bc1e
microsomes <tayyabdev@outlook.com>
2020-07-24 18:32:50 +0100

    routing issue

--- lib/v2/pages/buyflow/payment/paymentConfirmed.dart
+++ lib/v2/pages/buyflow/payment/paymentConfirmed.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:nxclone/pages/ticketwallet.dart';
+import 'package:nxclone/v2/pages/nxfront.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
-class PaymentConfirmed extends StatelessWidget{
+class PaymentConfirmed extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar:  AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.white,
         leading: InkWell(
             onTap: () {
@@ -28,117 +28,121 @@ class PaymentConfirmed extends StatelessWidget{
         children: <Widget>[
           Container(
             alignment: Alignment.center,
-            height: MediaQuery.of(context).size.height*0.7,
+            height: MediaQuery.of(context).size.height * 0.7,
             width: MediaQuery.of(context).size.width,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-               children: <Widget>[
-                Text("Transaction",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 20
+              children: <Widget>[
+                Text(
+                  "Transaction",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
                 ),
+                SizedBox(
+                  height: 2,
                 ),
-                SizedBox(height: 2,),
-                Text("Successful",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 20
+                Text(
+                  "Successful",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
                 ),
+                SizedBox(
+                  height: 3,
                 ),
-                SizedBox(height: 3,),
-                Icon(Icons.check,size: 100,color: Color.fromRGBO(81, 164, 27, 1),),
-                SizedBox(height: 20,),
-                Text("Please activate your ticket just \nbefore boarding",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 15
+                Icon(
+                  Icons.check,
+                  size: 100,
+                  color: Color.fromRGBO(81, 164, 27, 1),
                 ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Please activate your ticket just \nbefore boarding",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15),
                 ),
                 InkWell(
-                  onTap: (){
+                  onTap: () {
                     print("go to ticket page");
-                    Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => Ticketwallet()),
-                        );
+                    // Navigator.pushAndRemoveUntil(
+                    //       context,
+                    //       MaterialPageRoute(
+                    //           builder: (context) => Ticketwallet()),
+                    //     );
+                    Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(builder: (context) => Ticketwallet()),
+                        (Route<dynamic> route) => true);
                   },
-                                  child: Padding(
+                  child: Padding(
                     padding: const EdgeInsets.all(20),
                     child: Container(
                       alignment: Alignment.center,
-                            width: 380,
-                            height: 60,
-                            decoration: BoxDecoration(
-                                color: Color.fromRGBO(123, 26, 17, 1),
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [
-                                  BoxShadow(
-                                      color: Color.fromRGBO(215, 216, 218, 1),
-                                      offset: new Offset(0, 3))
-                                ]),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                              
-                              Text(
-                                "Go to Ticket Wallet",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 17),
-                              )
-                            ]),
-                          ),
+                      width: 380,
+                      height: 60,
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(123, 26, 17, 1),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromRGBO(215, 216, 218, 1),
+                                offset: new Offset(0, 3))
+                          ]),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Go to Ticket Wallet",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 17),
+                            )
+                          ]),
+                    ),
                   ),
                 ),
-
-                 InkWell(
-                   onTap: () async{
-                     const url = "https://www.nxrewards.com/";
-                      if (await canLaunch(url)) {
-                            await launch(url);
-                          } else {
-                            //throw 'Could not launch $url';
-                          }
-                   },
-                                    child: Padding(
-                    padding: const EdgeInsets.only(left:20,right:20,top:0),
+                InkWell(
+                  onTap: () async {
+                    const url = "https://www.nxrewards.com/";
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      //throw 'Could not launch $url';
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20, top: 0),
                     child: Container(
                       alignment: Alignment.center,
-                            width: 380,
-                            height: 60,
-                            decoration: BoxDecoration(
-                                color: Color.fromRGBO(65, 98, 53, 1),
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [
-                                  BoxShadow(
-                                      color: Color.fromRGBO(215, 216, 218, 1),
-                                      offset: new Offset(0, 3))
-                                ]),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                              
-                              Text(
-                                "NX Rewards Cashback",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 17),
-                              )
-                            ]),
-                          ),
+                      width: 380,
+                      height: 60,
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(65, 98, 53, 1),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color.fromRGBO(215, 216, 218, 1),
+                                offset: new Offset(0, 3))
+                          ]),
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "NX Rewards Cashback",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 17),
+                            )
+                          ]),
+                    ),
+                  ),
                 ),
-                 ),
               ],
             ),
           )
@@ -146,5 +150,4 @@ class PaymentConfirmed extends StatelessWidget{
       ),
     );
   }
-
 }
