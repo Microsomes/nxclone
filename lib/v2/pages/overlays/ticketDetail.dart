b2335f65ce6c9758e26eab241ddd70af37b7f87c
microsomes <tayyabdev@outlook.com>
2020-07-24 10:18:06 +0100

    first part of detail page done

--- lib/v2/pages/overlays/ticketDetail.dart
+++ lib/v2/pages/overlays/ticketDetail.dart
@@ -62,12 +62,70 @@ class TicketDetailState extends State<TicketDetail> {
                         child: Column(
                           children: <Widget>[
                             Expanded(
-                              child: ListView.builder(
-                                  itemCount: 10000,
-                                  itemBuilder: (context, index) {
-                                    return Text("$index");
-                                  }),
-                            ),
+                                child: ListView(
+                              children: <Widget>[
+                                Container(
+                                    padding: EdgeInsets.all(12),
+                                    alignment: Alignment.centerLeft,
+                                    height: 100,
+                                    width: MediaQuery.of(context).size.width,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(
+                                          "VALID UNTIL",
+                                          style: TextStyle(
+                                              color: Color.fromRGBO(
+                                                  110, 110, 110, 1),
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 16),
+                                        ),
+                                        SizedBox(
+                                          height: 16,
+                                        ),
+                                        Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Text(
+                                              "16 Oct 2020 23:59 ",
+                                              style: TextStyle(
+                                                  color: Color.fromRGBO(
+                                                      61, 61, 61, 1),
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 16),
+                                            ),
+                                            Expanded(
+                                              child: Container(),
+                                            ),
+                                            Container(
+                                              height: 20,
+                                              width: 90,
+                                              decoration: BoxDecoration(
+                                                  color: Color.fromRGBO(
+                                                      90, 90, 90, 1),
+                                                  borderRadius:
+                                                      BorderRadius.all(
+                                                          Radius.circular(1))),
+                                                          child: Center(
+                                                            child: Text("99 DAYS LEFT",
+                                                            textAlign: TextAlign.center,
+                                                            style: TextStyle(
+                                                              color: Colors.white,
+                                                              fontSize: 12,
+                                                              fontWeight: FontWeight.bold
+                                                            ),),
+                                                          ),
+                                            ),
+                                          ],
+                                        ),
+                                        SizedBox(height: 20,),
+                                        Container(height: 1,width: MediaQuery.of(context).size.height*0.8,color: Color.fromRGBO(231, 231, 231, 1),)
+                                      ],
+                                    ))
+                              ],
+                            )),
                             Row(
                               children: <Widget>[
                                 InkWell(
@@ -77,14 +135,18 @@ class TicketDetailState extends State<TicketDetail> {
                                   child: Row(
                                     children: <Widget>[
                                       IconButton(
-                                        icon: Icon(Icons.arrow_back_ios,color: Color.fromRGBO(77, 84, 137, 1),size: 18,),
+                                        icon: Icon(
+                                          Icons.arrow_back_ios,
+                                          color: Color.fromRGBO(77, 84, 137, 1),
+                                          size: 18,
+                                        ),
                                         onPressed: () {},
                                       ),
                                       Text(
                                         "Back",
                                         style: TextStyle(
-                                            color: Color.fromRGBO(
-                                                58, 63, 103, 1),
+                                            color:
+                                                Color.fromRGBO(58, 63, 103, 1),
                                             fontWeight: FontWeight.w500),
                                       )
                                     ],
@@ -98,14 +160,17 @@ class TicketDetailState extends State<TicketDetail> {
                                   child: Row(
                                     children: <Widget>[
                                       IconButton(
-                                        icon: Icon(Icons.sort,color: Color.fromRGBO(74, 81, 133, 1),),
+                                        icon: Icon(
+                                          Icons.sort,
+                                          color: Color.fromRGBO(74, 81, 133, 1),
+                                        ),
                                         onPressed: () {},
                                       ),
                                       Text(
                                         "Terms",
                                         style: TextStyle(
-                                            color: Color.fromRGBO(
-                                                86, 94, 155, 1),
+                                            color:
+                                                Color.fromRGBO(86, 94, 155, 1),
                                             fontWeight: FontWeight.w500),
                                       )
                                     ],
