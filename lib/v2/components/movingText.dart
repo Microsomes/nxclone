8d0a5b1bb25a33393a8841582456902a0b89d30f
microsomes <tayyabdev@outlook.com>
2020-07-24 09:14:32 +0100

    moving test working

--- lib/v2/components/movingText.dart
+++ lib/v2/components/movingText.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 
 class MovingText extends StatefulWidget {
   final String textContent;
@@ -13,21 +14,63 @@ class MovingText extends StatefulWidget {
 }
 
 class MovingTextState extends State<MovingText> {
+  double currentTextPos = 0;
+
+  void startAnimation() {}
+
+  void init() {}
+
+  Widget _buildMarquee() {
+    return Marquee(
+      text: widget.textContent,
+      style: TextStyle(
+          letterSpacing: 0.3,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 16),
+      pauseAfterRound: Duration(seconds: 3),
+      blankSpace: 30,
+      showFadingOnlyWhenScrolling: true,
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.init();
+  }
+
   @override
   Widget build(BuildContext context) {
     var tc = widget.textContent;
     // TODO: implement build
     return Container(
-      alignment: Alignment.centerLeft,
-      width: MediaQuery.of(context).size.width,
-       child: Text(
-        "$tc",
-        style: TextStyle(
-            letterSpacing: 0.3,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 16),
-      ),
-    );
+        alignment: Alignment.centerLeft,
+        width: MediaQuery.of(context).size.width,
+        height: 50,
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: <Widget>[
+            // Positioned(
+            //   left: currentTextPos,
+            //   top: 0,
+            //         child: Container(
+            //      alignment: Alignment.centerLeft,
+            //     height: 50,
+            //     width: MediaQuery.of(context).size.width,
+            //      child: Text(
+            //        "$tc",
+            //        style: TextStyle(
+            //            letterSpacing: 0.3,
+            //            color: Colors.white,
+            //            fontWeight: FontWeight.bold,
+            //            fontSize: 16),
+            //      ),
+            //   ),
+            // ),
+            _buildMarquee(),
+          ],
+        ));
   }
 }
