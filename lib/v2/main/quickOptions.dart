40319b54b2c8b33d4e8fc0ac803f1f5463af6841
microsomes <tayyabdev@outlook.com>
2020-08-11 07:40:02 +0100

    linked up the options again

--- lib/v2/main/quickOptions.dart
+++ lib/v2/main/quickOptions.dart
@@ -42,7 +42,7 @@ class _QuickOptionState extends State<QuickOptions> {
       "title": "NX-Animations",
       "icon": Icons.lightbulb_outline,
       "asset": "images/knowledge1.png",
-      "useIcon":true
+      "useIcon": true
     });
     //quickOptions= quickOptions.reversed.toList();
   }
@@ -50,11 +50,12 @@ class _QuickOptionState extends State<QuickOptions> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: GridView.count(
-            crossAxisCount: 2,
-            children: List.generate(quickOptions.length, (index) {
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Container(
+        child: ListView.builder(
+            itemCount: quickOptions.length,
+            itemBuilder: (context, index) {
               return InkWell(
                 onTap: () {
                   if (index == 0) {
@@ -74,63 +75,107 @@ class _QuickOptionState extends State<QuickOptions> {
                     );
                   }
 
-                  if(index==4){
+                  if (index == 4) {
                     AnimationOverlay().display(context);
                   }
                 },
-                child: Container(
-                  height: 2,
-                  width: MediaQuery.of(context).size.width / 3,
-                  margin: EdgeInsets.all(3),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10), boxShadow: []),
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(110),
-                        color: Colors.lightGreen,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.green.withOpacity(0.4),
-                            spreadRadius: 2,
-                            blurRadius: 7,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ]),
-                    child: Column(
-                      children: <Widget>[
-                        Expanded(
-                          child: Container(
-                            child: Center(
-                              child: quickOptions[index]['useIcon']==true ? Container(
-                                child: Center(
-                                  child: Icon(quickOptions[index]['icon'],size: 60,),
-                                ),
-                              ) : Image.asset(
-                                quickOptions[index]['asset'],
-                                width: MediaQuery.of(context).size.width * 0.15,
-                              ),
-                            ),
-                            decoration: BoxDecoration(),
-                          ),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width * 0.2,
-                          child: Text(
-                            quickOptions[index]['title'],
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 13,
-                            ),
-                          ),
-                        ),
-                        SizedBox(height: 20,),
-                      ],
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Text(
+                      quickOptions[index]['title'][0],
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     ),
+                    backgroundColor: Colors.transparent,
                   ),
+                  title: Text(quickOptions[index]['title']
+                      .substring(1, quickOptions[index]['title'].length)),
                 ),
               );
-            })));
+            }),
+      ),
+      // child: GridView.count(
+      //     crossAxisCount: 1,
+      //     children: List.generate(quickOptions.length, (index) {
+      //       return InkWell(
+      //         onTap: () {
+      //           if (index == 0) {
+      //             Navigator.push(
+      //               context,
+      //               MaterialPageRoute(builder: (context) => LandingPage()),
+      //             );
+      //           } else if (index == 1) {
+      //             Navigator.push(
+      //               context,
+      //               MaterialPageRoute(builder: (context) => DefaultTicket()),
+      //             );
+      //           } else if (index == 2) {
+      //             Navigator.push(
+      //               context,
+      //               MaterialPageRoute(builder: (context) => Ejection()),
+      //             );
+      //           }
+
+      //           if(index==4){
+      //             AnimationOverlay().display(context);
+      //           }
+      //         },
+      //         child: Container(
+      //           height: 2,
+      //           width: MediaQuery.of(context).size.width / 3,
+      //           margin: EdgeInsets.all(3),
+      //           decoration: BoxDecoration(
+      //               borderRadius: BorderRadius.circular(10), boxShadow: []),
+      //           child: Container(
+      //             decoration: BoxDecoration(
+      //                 borderRadius: BorderRadius.circular(10),
+      //                 color: Colors.lightGreen,
+      //                 boxShadow: [
+      //                   BoxShadow(
+      //                     color: Colors.green.withOpacity(0.4),
+      //                     spreadRadius: 2,
+      //                     blurRadius: 7,
+      //                     offset: Offset(0, 3), // changes position of shadow
+      //                   ),
+      //                 ]),
+      //             child: Column(
+      //               children: <Widget>[
+      //                 Expanded(
+      //                   child: Container(
+      //                     child: Center(
+      //                       child: quickOptions[index]['useIcon']==true ? Container(
+      //                         child: Center(
+      //                           child: Icon(quickOptions[index]['icon'],size: 60,),
+      //                         ),
+      //                       ) : Image.asset(
+      //                         quickOptions[index]['asset'],
+      //                         width: MediaQuery.of(context).size.width * 0.15,
+      //                       ),
+      //                     ),
+      //                     decoration: BoxDecoration(),
+      //                   ),
+      //                 ),
+      //                 Container(
+      //                   width: MediaQuery.of(context).size.width * 0.2,
+      //                   child: Text(
+      //                     quickOptions[index]['title'],
+      //                     textAlign: TextAlign.center,
+      //                     style: TextStyle(
+      //                       color: Colors.white,
+      //                       fontWeight: FontWeight.bold,
+      //                       fontSize: 13,
+      //                     ),
+      //                   ),
+      //                 ),
+      //                 SizedBox(height: 20,),
+      //               ],
+      //             ),
+      //           ),
+      //         ),
+      //       );
+      //     })));
+    );
   }
 }
