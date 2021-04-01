d552ba1ba6d9639edf0bb4efac3e443a929b1408
tayyab <tayyabdev@outlook.com>
2021-04-02 00:24:52 +0100

    setup imroved

--- lib/setupMain.dart
+++ lib/setupMain.dart
@@ -1,20 +1,23 @@
+import 'package:BubbleGum/piHome.dart';
 import 'package:flutter/material.dart';
 
 import 'package:animated_text/animated_text.dart';
+import 'package:cube_transition/cube_transition.dart';
 
 import 'v2/helper/NxHelp.dart';
 
-class StartScreenSetup extends StatefulWidget{
+class StartScreenSetup extends StatefulWidget {
   @override
   _StartScreenSetupState createState() => _StartScreenSetupState();
 }
 
 class _StartScreenSetupState extends State<StartScreenSetup> {
-
+  AnimatedTextController _controller;
   @override
   void initState() {
     super.initState();
-     NXHelp().runInit().then((value) {
+    _controller = AnimatedTextController.play;
+    NXHelp().runInit().then((value) {
       print("imported");
     });
   }
@@ -25,22 +28,32 @@ class _StartScreenSetupState extends State<StartScreenSetup> {
       backgroundColor: Colors.white,
       body: Center(
         child: AnimatedText(
-                alignment: Alignment.center,
-                speed: Duration(milliseconds: 1000),
-                controller: AnimatedTextController.play,
-                displayTime: Duration(milliseconds: 1000),
-                wordList: ['BubbleGum.', 'V5.', 'has.', 'arrived.',"Enjoy."],
-                textStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 55,
-                    fontWeight: FontWeight.w700),
-                    onAnimate: (index) {
-                print("Animating index:" + index.toString());
-                },
-                 onFinished: () {
-                  print("Animtion finished");
-                },
-              ),
+          alignment: Alignment.center,
+          speed: Duration(milliseconds: 1000),
+          controller: _controller,
+          displayTime: Duration(milliseconds: 400),
+          wordList: ['BubbleGUM.', 'V5.', '.'],
+          textStyle: TextStyle(
+              color: Colors.black, fontSize: 55, fontWeight: FontWeight.w700),
+          onAnimate: (index) {
+            if (index == 2) {
+              setState(() {
+                _controller = AnimatedTextController.stop;
+              });
+
+              Future.delayed(Duration(seconds: 1), () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context)=>PiHome()));
+              });
+            }
+          },
+          onFinished: () {
+            //  Navigator.pop(context);
+
+            //  Navigator.push(context, MaterialPageRoute(builder: (context)=>PiHome()));
+          },
+        ),
       ),
     );
   }
