f5981a436f02f8c6fcc86bc059b059000fdec7bc
Tayyab Javed <tayyabdev@outlook.com>
2021-05-06 05:51:10 +0100

    sdfdsf

--- lib/v2/pages/menupage/socialMediaPage.dart
+++ lib/v2/pages/menupage/socialMediaPage.dart
@@ -26,41 +26,52 @@ void launchURL(String url) async {
 
     return Scaffold(
         backgroundColor: Color.fromRGBO(167, 27, 26, 1),
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          leading: InkWell(
-              onTap: () {
-                Navigator.pop(context, true);
-              },
-              child: PreferredSize(
-                  preferredSize: Size.fromHeight(500),
-                  child: Image.asset("images/leftarrow.png", width: 2))),
-          title: Center(
-            child: Text(
-              "Social media",
-              style: TextStyle(color: Color.fromRGBO(189, 156, 106, 1)),
-            ),
-          ),
-        ),
+        // appBar: AppBar(
+        //   backgroundColor: Colors.white,
+        //   leading: InkWell(
+        //       onTap: () {
+        //         Navigator.pop(context, true);
+        //       },
+        //       child: PreferredSize(
+        //           preferredSize: Size.fromHeight(500),
+        //           child: Image.asset("images/leftarrow.png", width: 2))),
+        //   title: Center(
+        //     child: Text(
+        //       "Social media",
+        //       style: TextStyle(color: Color.fromRGBO(189, 156, 106, 1)),
+        //     ),
+        //   ),
+        // ),
         body: Container(
             height: double.infinity,
             width: sizeW,
             color: Color.fromRGBO(167, 27, 26, 1),
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 17),
-                NavItem(
-                  onTap: (){
-                    launchURL("https://m.facebook.com/nxwestmidlands/");
-                  },
-                    name: "Facebook", image: Image.asset("images/v3/facebook.png",width: 20,)),
-                SizedBox(height: 8),
-                NavItem(
-                  onTap: (){
-                    launchURL("https://twitter.com/nxwestmidlands");
-                  },
-                    name: "Twitter", image: Image.asset("images/v3/twitter.png",width: 20,))
-              ],
+            child: SafeArea(
+                          child: Column(
+                children: <Widget>[
+                  PhysicalModel(
+                    elevation: 2,
+                    color: Colors.red,
+                    shadowColor: Colors.pink,
+                                      child: Container(
+                      height: 55,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 17),
+                  NavItem(
+                    onTap: (){
+                      launchURL("https://m.facebook.com/nxwestmidlands/");
+                    },
+                      name: "Facebook", image: Image.asset("images/v3/facebook.png",width: 20,)),
+                  SizedBox(height: 8),
+                  NavItem(
+                    onTap: (){
+                      launchURL("https://twitter.com/nxwestmidlands");
+                    },
+                      name: "Twitter", image: Image.asset("images/v3/twitter.png",width: 20,))
+                ],
+              ),
             )));
   }
 }
