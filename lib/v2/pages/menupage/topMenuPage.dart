f950922ebbd5df49bd3f47fb13f6a98348135035
United Kingdom javed <unitedkingdomjaved@Uniteds-MacBook-Air.local>
2022-09-19 19:48:07 +0100

    utilities page done

--- lib/v2/pages/menupage/topMenuPage.dart
+++ lib/v2/pages/menupage/topMenuPage.dart
@@ -23,116 +23,204 @@ void launchURL(String url) async {
 }
 
 class UtilitiesMenuState extends State<UtilitiesMenu> {
-
-
   var sections = [
     {
-      "section":"Account",
-      "links":[
+      "section": "Account",
+      "links": [
         {
-          "type":"link",
-          "label":"My Profile",
-          "icon":"images/front/account.svg"
+          "type": "link",
+          "label": "My Profile",
+          "icon": "images/front/account.svg",
+          "action": (){}
         }
       ]
     },
     {
-      "section":"Tools",
-      "links":[
+      "section": "Tools",
+      "links": [
         {
-          "type":"link",
-          "label":"nxbus.co.uk",
-          "icon":"images/front/website.svg"
-        },
-         {
-          "type":"link",
-          "label":"Social media",
-          "icon":"images/front/social-media.svg"
+          "type": "link",
+          "label": "nxbus.co.uk",
+          "icon": "images/front/website.svg",
+          "action": (){}
         },
         {
-          "type":"link",
-          "label":"Payzone Barcode",
-          "icon":"images/front/DBarcode.svg"
+          "type": "link",
+          "label": "Social media",
+          "icon": "images/front/social-media.svg",
+          "action": (){}
+        },
+        {
+          "type": "link",
+          "label": "Payzone Barcode",
+          "icon": "images/front/DBarcode.svg",
+          "action": (){}
         }
       ]
     }
   ];
 
-
   @override
   Widget build(BuildContext context) {
     final sizeW = MediaQuery.of(context).size.width;
     return Scaffold(
-      
-        body: Container(
-          
-      height: double.infinity,
-      width: sizeW,
-      color: Colors.white,
-      child: Container(
-        child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              PhysicalModel(
-                  elevation: 0,
-                  color: Colors.black,
-                  child: Stack(
-                    alignment: Alignment.centerLeft,
-                    children: [
-                      Container(
-                        child: Center(
-                          child: Text("Utilities",
-                              style: GoogleFonts.roboto(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 16)),
-                        ),
-                        height: 55,
-                        color: Colors.white,
-                      ),
-                      GestureDetector(
-                          onTap: () {
-                            Navigator.pop(context);
-                          },
-                          child: Container(
-                            color: Colors.white,
-                            width: 90,
-                            height: 50,
+        backgroundColor: Color.fromRGBO(168, 27, 28, 1),
+        body: Center(
+            child: Column(
+          children: [
+            Container(
+                width: double.infinity,
+                height: 80,
+                color: Colors.white,
+                child: Stack(alignment: Alignment.center, children: [
+                  Positioned(
+                      left: 0,
+                      top: 50,
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Container(
+                          width: 100,
+                          child: Positioned(
+                            top: 4,
                             child: Row(
                               children: [
-                                SizedBox(width: 5,),
-                                SvgPicture.asset("images/front/back.svg",
-                                width: 18,
+                                SizedBox(
+                                  width: 5,
                                 ),
-                                SizedBox(width: 2,),
-                                Text("Back",
-                                style: GoogleFonts.roboto(
-                                  fontSize: 16
+                                SvgPicture.asset(
+                                  "images/front/back.svg",
+                                  width: 21,
                                 ),
+                                SizedBox(
+                                  width: 2,
+                                ),
+                                Text(
+                                  "Back",
+                                  style: GoogleFonts.roboto(fontSize: 16),
                                 )
                               ],
                             ),
-                          )),
-                    ],
-                  )),
-               SizedBox(
-                height: 10,
-              ),
-              Expanded(
-                child: Container(
-                  color: Color.fromRGBO(168, 27, 26, 1),
-                  child: CustomScrollView(
-                    slivers: [
-                      
-                    ],
-                  ),
+                          ),
+                        ),
+                      )),
+                  Positioned(
+                    top: 50,
+                    child: Text("Utilities",
+                        style: GoogleFonts.roboto(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16)),
+                  )
+                ])),
+            Expanded(
+              child:CustomScrollView(
+                slivers: [
+                  SliverFillRemaining(
+                    child:  UtilitiesList(sections: sections),
+                  )
+                ],
+              )
+            )
+          ],
+        )));
+  }
+}
+
+class UtilitiesList extends StatelessWidget {
+  const UtilitiesList({
+    Key key,
+    @required this.sections,
+  }) : super(key: key);
+
+  final List<Map<String, Object>> sections;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.transparent,
+      child: Column(
+        children: [
+          SizedBox(height: 30,),
+          for (var i = 0; i < sections.length; i++)
+            Builder(builder: (ctx) {
+              List links = sections[i]['links'];
+              return Container(
+                padding: EdgeInsets.only(left: 10),
+                margin: EdgeInsets.only(top: i ==0 ? 0 :20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      sections[i]['section'],
+                      style: GoogleFonts.roboto(
+                          fontSize: 25,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10,),
+                    for (var l = 0; l < links.length; l++)
+                      Container(
+                        child: Row(
+                          children: [
+                            Container(
+                                margin: EdgeInsets.only(right: 10),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20.0),
+                                  child: SvgPicture.asset(
+                                    links[l]['icon'],
+                                    // color: links[i]['iconColor'],
+                                    height: 25,
+                                  ),
+                                )),
+                            Expanded(
+                              child: Container(
+                                child: Text(
+                                  links[l]['label'],
+                                  style: GoogleFonts.roboto(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: 60,
+                              child: Icon(
+                                Icons.chevron_right,
+                                size: 35,
+                                color: Color.fromRGBO(172, 22, 32, 1),
+                              ),
+                            )
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Color.fromRGBO(158, 25, 26, 1),
+                                spreadRadius: 1,
+                                blurRadius: 0,
+                                offset: Offset(0,
+                                    1), // changes position of shadow
+                              ),
+                            ],
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(7)),
+                        height: 45,
+                        margin: EdgeInsets.only(
+                            top: l == 0 ? 0 : 20,
+                            left: 0,
+                            right: 10),
+                      )
+                  ],
                 ),
-              )    
-            ])
-          
-        ),
+              );
+            })
+        ],
       ),
-    ));
+    );
   }
 }
 
