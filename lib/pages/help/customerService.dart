152947449fe0728ead8524775b60deec8cd54a9f
United Kingdom javed <unitedkingdomjaved@Uniteds-MacBook-Air.local>
2022-09-21 15:41:47 +0100

    call button

--- lib/pages/help/customerService.dart
+++ lib/pages/help/customerService.dart
@@ -1,74 +1,188 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+class CustomerServicePage extends StatefulWidget {
+  @override
+  _ApplicationInfoPageState createState() => _ApplicationInfoPageState();
+}
 
-class CustomerServicePage extends StatelessWidget{
+class _ApplicationInfoPageState extends State<CustomerServicePage> {
+  String applicationIdentifier;
+  String accountIdentifier;
+  String platformName;
+  String deviceModel;
+  int buildNumber;
+
+  var options = [
+    {"label": "Phone", "val": "0121 254 7272"},
+    {"label": "Website", "val": "www.nxbus.co.uk/"},
+    {
+      "type": "custom",
+      "label": "",
+      "val":
+          "Contact us if any issues arises. Please include your application ID in all requests. This information can be found within the application under Help > App Info."
+    }
+  ];
+
+  @override
+  void initState() {
+    setState(() {
+      buildNumber = 1597997279;
+      applicationIdentifier = "AAXPEINFLH7";
+    });
+
+    // DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+
+    // deviceInfo.androidInfo.then((value) {
+    //   AndroidDeviceInfo dv = value;
+    //   setState(() {
+    //     deviceModel = dv.device;
+    //     platformName=dv.version.sdkInt.toString();
+    //   });
+    // });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: InkWell( onTap: (){
+        body: Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+              width: double.infinity,
+              height: 90,
+              color: Color.fromRGBO(168, 28, 25, 1),
+              child: Stack(alignment: Alignment.center, children: [
+                Positioned(
+                    left: 0,
+                    top: 60,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        width: 100,
+                        child: Positioned(
+                          top: 4,
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 5,
+                              ),
+                              SvgPicture.asset(
+                                "images/front/back.svg",
+                                width: 21,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 2,
+                              ),
+                              Text(
+                                "Back",
+                                style: GoogleFonts.roboto(
+                                    fontSize: 16, color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )),
+                Positioned(
+                  top: 60,
+                  child: Text("Customer service",
+                      style: GoogleFonts.roboto(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 16)),
+                )
+              ])),
+          Expanded(
+              child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                child: AppInfo(
+                  options: options,
+                ),
+              )
+            ],
+          )),
+           Container(
+            child: Center(
+              child: Text(
+                "Call",
+                style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.bold, fontSize: 17),
+              ),
+            ),
+            height: 55,
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(194, 154, 99, 1),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromRGBO(199, 199, 199, 1),
+                    spreadRadius: 0,
+                    blurRadius: 2,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(10)),
+            margin: EdgeInsets.only(left: 18, right: 18, bottom: 7),
+          )
 
-    Navigator.pop(context,true);
-
-        },child: PreferredSize( preferredSize: Size.fromHeight(500),child: Image.asset("images/leftarrow.png",width:4))),
-        title: Center(child: Text("Customer service", style: TextStyle(color: Color.fromRGBO(189, 156, 106, 1)),),),
+        ],
       ),
-      body: Center(
-        child: Column(
-           children: <Widget>[
-             SizedBox(
-               height: 50,
-             ),
-             Text("Phone: 0121 254 7272", style: TextStyle(
-               fontWeight: FontWeight.w600,
-               fontSize: 20
-             ),),
-             SizedBox(height: 5,),
-          
-            SizedBox(height: 25,),
+    ));
+  }
+}
 
-           Text("Website", style: TextStyle(
-               fontWeight: FontWeight.w600,
-               fontSize: 20
-             ),),
-             SizedBox(height: 5,),
-          Text("www.nxbus.co.uk/", style: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 18
-          ),),
+class AppInfo extends StatelessWidget {
+  const AppInfo({
+    Key key,
+    @required this.options,
+  }) : super(key: key);
 
-          SizedBox(height: 15,),
+  final List<Map<String, String>> options;
 
-
-          
-           Padding(
-             padding: const EdgeInsets.all(10.0),
-             child: Center(
-               child: Text("Contact us if any issues arise. Please include your application ID in all requests. This information can be found within the application under Help > App Info.", style: TextStyle(
-                   fontWeight: FontWeight.w500,
-                   fontSize: 16
-                 ),),
-             ),
-           ),
-             SizedBox(height: 5,),
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          for (var i = 0; i < options.length; i++)
+            Builder(builder: (ctx) {
+              return Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(left: 20, top: 5, bottom: 5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      options[i]['label'],
+                      style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w800, fontSize: 15),
+                    ),
+                    Text(
+                      options[i]['val'],
+                      style: GoogleFonts.roboto(fontSize: 15),
+                    ),
+                    Divider()
+                  ],
+                ),
+              );
+            }),
+          Expanded(
+            child: Container(),
+          ),
          
-
-          
-         
-    
-
-
-
-
-
-           ],
-        ),
-      )
+        ],
+      ),
     );
   }
-
 }
