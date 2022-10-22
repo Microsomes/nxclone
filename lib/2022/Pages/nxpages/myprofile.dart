50cc68230e65e02015d3be350cc39d7d02a9cc46
United Kingdom javed <unitedkingdomjaved@Uniteds-MacBook-Air.local>
2022-10-23 00:26:21 +0100

    custom scroll view

--- lib/2022/Pages/nxpages/myprofile.dart
+++ lib/2022/Pages/nxpages/myprofile.dart
@@ -4,16 +4,13 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../../components/form/NXFormInput.dart';
 
-
-class MyProfile extends StatefulWidget{
+class MyProfile extends StatefulWidget {
   @override
   State<MyProfile> createState() => _MyProfileState();
 }
 
 class _MyProfileState extends State<MyProfile> {
-
-
-  bool isLogin = false;
+  bool isLogin = true;
 
   @override
   Widget build(BuildContext context) {
@@ -21,119 +18,162 @@ class _MyProfileState extends State<MyProfile> {
       backgroundColor: Color.fromRGBO(169, 26, 25, 1),
       body: Column(
         children: [
-           Container(
-                width: double.infinity,
-                height: 70,
-                color: Colors.transparent,
-                child: Stack(alignment: Alignment.center, children: [
-                  Positioned(
-                      right: 0,
-                      top: 35,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: Container(
-                          width: 100,
-                          child: Positioned(
-                            child: Row(
-                              children: [
-                                SizedBox(
-                                  width: 60,
-                                ),
-                                Icon(Icons.close,
+          Container(
+              width: double.infinity,
+              height: 73,
+              color: Colors.transparent,
+              child: Stack(alignment: Alignment.center, children: [
+                Positioned(
+                    right: 0,
+                    top: 38,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        width: 100,
+                        child: Positioned(
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 60,
+                              ),
+                              Icon(
+                                Icons.close,
                                 size: 28,
                                 color: Colors.white.withOpacity(0.95),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )),
+                Positioned(
+                  top: 44,
+                  child: Text("My profile",
+                      style: GoogleFonts.roboto(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16)),
+                )
+              ])),
+          Expanded(
+            child: Container(
+                color: Colors.white,
+                child: CustomScrollView(
+                  slivers: [
+                    SliverFillRemaining(
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 50,
+                            color: Color.fromRGBO(139, 0, 8, 1),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        isLogin = true;
+                                      });
+                                    },
+                                    child: Container(
+                                      child: Center(
+                                        child: Text(
+                                          "Log in",
+                                          style: GoogleFonts.roboto(
+                                              fontWeight: FontWeight.w500,
+                                              color: isLogin == false
+                                                  ? Colors.white
+                                                  : Colors.black),
+                                        ),
+                                      ),
+                                      decoration: BoxDecoration(
+                                          color: isLogin == false
+                                              ? Color.fromRGBO(139, 0, 8, 1)
+                                              : Colors.white,
+                                          borderRadius: BorderRadius.only(
+                                            topRight: Radius.circular(5),
+                                          )),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        isLogin = false;
+                                      });
+                                    },
+                                    child: Container(
+                                      child: Center(
+                                        child: Text(
+                                          "New Account",
+                                          style: GoogleFonts.roboto(
+                                              fontWeight: FontWeight.w500,
+                                              color: isLogin == true
+                                                  ? Colors.white
+                                                  : Colors.black),
+                                        ),
+                                      ),
+                                      color: isLogin == true
+                                          ? Color.fromRGBO(139, 0, 8, 1)
+                                          : Colors.white,
+                                    ),
+                                  ),
                                 )
                               ],
                             ),
                           ),
-                        ),
-                      )),
-                  Positioned(
-                    top: 40,
-                    child: Text("My profile",
-                        style: GoogleFonts.roboto(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16)),
-                  )
-                ])),
-                Container(
-                  height: 50,
-                  color: Color.fromRGBO(139, 0, 8, 1),
-                  child: Row(children: [
-                    Expanded(
-                      child: GestureDetector(
-                        onTap: (){
-                          setState(() {
-                            isLogin = true;
-                          });
-                        },
-                        child: Container(
-                          child: Center(
-                            child: Text("Log in",
-                            style: GoogleFonts.roboto(
-                              fontWeight: FontWeight.w500,
-                              color: isLogin == false ? Colors.white : Colors.black
-                            ),
-                            ),
-                          ),
-                          decoration: BoxDecoration(
-                            color: isLogin == false ? Color.fromRGBO(139, 0, 8, 1): Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(5),
-                      
-                            )
-                          ),
-                        ),
-                      ),
-                    ),
-                     Expanded(
-                      child: GestureDetector(
-                        onTap: () {
-                            setState(() {
-                            isLogin = false;
-                          });
-                        },
-                        child: Container(
-                          child: Center(
-                            child: Text("New Account",
-                            style: GoogleFonts.roboto(
-                              fontWeight: FontWeight.w500,
-                              color: isLogin == true ? Colors.white : Colors.black
-                            ),
-                            ),
-                          ),
-                          color: isLogin == true ? Color.fromRGBO(139, 0, 8, 1): Colors.white,
-                        ),
+                          LoginForm()
+                        ],
                       ),
                     )
-                  ],),
-                ),
-          Expanded(
-            child:Container(
-              color: Colors.white,
-              child: SingleChildScrollView(
-                child: Form(
-                  child: Column(
-                    children: [
-                      NXFormInputComponent(
-                        label: "Email",
-                        tip: "",
-                        validator: (String val){
-                          if(val.length<=5){
-                            return "Please enter a valid email";
-                          }
-                          return null;
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+                  ],
+                )),
           )
+        ],
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          NXFormInputComponent(
+            label: "Email",
+            tip: "",
+            validator: (String val) {
+              if (val.length <= 5) {
+                return "Please enter a valid email";
+              }
+              return null;
+            },
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          NXFormInputComponent(
+            label: "Password",
+            tip: "",
+            validator: (String val) {
+              if (val.length <= 5) {
+                return "Please enter a valid password";
+              }
+              return null;
+            },
+          ),
         ],
       ),
     );
--- lib/2022/components/form/NXFormInput.dart
+++ lib/2022/components/form/NXFormInput.dart
@@ -1,19 +1,13 @@
 import "package:flutter/material.dart";
-
-
-
+import 'package:google_fonts/google_fonts.dart';
 
 class NXFormInputComponent extends StatefulWidget {
+  final String label;
+  final String tip;
+  final Function validator;
 
-    final String label;
-    final String tip;
-    final Function validator;
-
-    NXFormInputComponent({
-      @required this.label,
-      @required this.tip,
-      @required this.validator
-    });
+  NXFormInputComponent(
+      {@required this.label, @required this.tip, @required this.validator});
 
   @override
   State<NXFormInputComponent> createState() => _NXFormInputComponentState();
@@ -23,9 +17,27 @@ class _NXFormInputComponentState extends State<NXFormInputComponent> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      width: double.infinity,
-      color: Colors.black,
+      color: Colors.transparent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 22),
+            child: Text(
+              widget.label,
+              style: GoogleFonts.roboto(fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Container(
+            height: 50,
+            margin: EdgeInsets.only(left: 20, right: 20),
+            width: double.infinity,
+            decoration: BoxDecoration(
+                border: Border.all(), borderRadius: BorderRadius.circular(10)),
+          )
+        ],
+      ),
     );
   }
 }
